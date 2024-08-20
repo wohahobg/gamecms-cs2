4e3d8d698a22d3676ac847ce8d1b9de4b8af54c3
Aleksandar Ovcharov <aleksandar.ovcharov@gamecms.org>
2024-08-21 00:07:03 +0100

    Changed Version

--- GameCMSPlugin.cs
+++ GameCMSPlugin.cs
@@ -19,7 +19,7 @@
     public sealed partial class GameCMSPlugin : BasePlugin, IPluginConfig<GameCMSConfig>
     {
         public override string ModuleName => "GameCMS.ORG";
-        public override string ModuleVersion => "1.0.5";
+        public override string ModuleVersion => "1.0.6";
         public override string ModuleAuthor => "GameCMS.ORG (Wohaho)";
         public override string ModuleDescription => "Plugin that allows you to connect your CS2 Server with the platform. Made with love. Keep it simple - GameCMS.ORG";
         public GameCMSConfig Config { get; set; } = new();
